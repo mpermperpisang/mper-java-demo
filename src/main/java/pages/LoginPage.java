54b53fa31c2f446edfc26c18adde43ac7e1a3dd8
Ferawati Hartanti Pratiwi <mpermperpisang@gmail.com>
2025-05-20 16:04:03 +0700

    fix: page pbject & properties

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,7 +1,8 @@
 package pages;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import utils.PropertyLoader;
 
 import java.util.Properties;
@@ -13,31 +14,39 @@ public class LoginPage {
     private By usernameField;
     private By passwordField;
     private By loginButton;
+    private By errorMessage;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
 
-        // Dynamically determine properties filename: "Login.properties"
+        // Dynamically load properties file e.g. "Login.properties"
         String propertiesFileName = this.getClass().getSimpleName().replace("Page", "") + ".properties";
-
-        // Load properties file (e.g. Login.properties)
         locators = PropertyLoader.loadProperties(propertiesFileName);
 
-        // Initialize locators from properties
+        // Initialize locators from properties file
         usernameField = By.cssSelector(locators.getProperty("USERNAME_FIELD"));
         passwordField = By.cssSelector(locators.getProperty("PASSWORD_FIELD"));
         loginButton = By.cssSelector(locators.getProperty("LOGIN_BUTTON"));
+        errorMessage = By.cssSelector(locators.getProperty("ERROR_MESSAGE"));
     }
 
     public void enterUsername(String username) {
-        driver.findElement(usernameField).sendKeys(username);
+        WebElement element = driver.findElement(usernameField);
+        element.clear();
+        element.sendKeys(username);
     }
 
     public void enterPassword(String password) {
-        driver.findElement(passwordField).sendKeys(password);
+        WebElement element = driver.findElement(passwordField);
+        element.clear();
+        element.sendKeys(password);
     }
 
     public void clickLogin() {
         driver.findElement(loginButton).click();
+    }
+
+    public String getErrorMessage() {
+        return driver.findElement(errorMessage).getText();
     }
 }
